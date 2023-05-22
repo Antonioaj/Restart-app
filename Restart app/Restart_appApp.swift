0da4b49ea64febdb0b7ee8145be2f79d68b6cd04
AntonioAj <antonioaj@hotmail.co.uk>
2023-05-22 16:58:13 +0100

    Initial Commit

---
+++ Restart app/Restart_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_appApp.swift
+//  Restart app
+//
+//  Created by Antonio Ajewole on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Restart_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
